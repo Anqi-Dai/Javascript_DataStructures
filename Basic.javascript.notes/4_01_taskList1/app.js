ebeb040005bb4ba8504949e5fe6a57853493173a
Anqi-Dai <adai@bu.edu>
2018-11-14 22:21:33 -0500

    Finished the task list app, learned so much

--- Basic.javascript.notes/4_01_taskList1/app.js
+++ Basic.javascript.notes/4_01_taskList1/app.js
@@ -1,3 +1,9 @@
+/* 
+The task list app should do the following:
+Add, remove, filter, clear the tasks
+And the tasks should be added and removed to the local storage (LS) too, so that the tasks still persist even if the page reloads.
+*/
+
 // Define UI variables
 const form = document.querySelector("form");
 const taskInput = document.querySelector("#task");
@@ -9,6 +15,9 @@ addEventListeners();
 
 // functions
 function addEventListeners() {
+  // Right after the DOM is loaded we wanna show the tasks in the local storage so that even though we refresh we still will be able to see them
+  document.addEventListener("DOMContentLoaded", getTasks);
+
   // Add task when submitting a task through form
   form.addEventListener("submit", addTask);
 
@@ -40,17 +49,73 @@ function addTask(e) {
   li.appendChild(link);
   taskList.appendChild(li);
 
+  // Add task to the LS
+  storeTaskInLS(taskInput.value);
+
   //clear the input
   taskInput.value = "";
 
   e.preventDefault();
 }
 
+// Store task in local storage
+function storeTaskInLS(task) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  // push the new task to the tasks array
+  tasks.push(task);
+
+  // set it back to LS in strings(you have to choose the specific address to see it in chrome tools)
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// get the tasks from the local storage
+function getTasks(e) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+
+  tasks.forEach(function(task) {
+    const li = document.createElement("li");
+    li.className = "collection-item";
+    li.appendChild(document.createTextNode(task));
+
+    const link = document.createElement("a");
+    link.className = "delete-item secondary-content";
+    link.innerHTML = '<i class = "fa fa-remove"></i>';
+
+    li.appendChild(link);
+    taskList.appendChild(li);
+  });
+}
+
 function removeTask(e) {
   if (e.target.parentElement.classList.contains("delete-item")) {
-    confirm("Are you sure?");
-    e.target.parentElement.parentElement.remove();
+    if (confirm("Are you sure?")) {
+      e.target.parentElement.parentElement.remove();
+
+      removeTaskFromLS(e.target.parentElement.parentElement.textContent);
+    }
   }
+}
+
+// Remove the task from LS if remove it in the UI
+function removeTaskFromLS(taskItem) {
+  let tasks = JSON.parse(localStorage.getItem("tasks"));
+  tasks.forEach(function(task, index) {
+    if (taskItem === task) {
+      tasks.splice(index, 1);
+    }
+  });
+  // And then you have to set it back to the local storage to make the change!
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 function clearTasks(e) {
@@ -60,6 +125,9 @@ function clearTasks(e) {
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
+
+  // remove tasks in LS too
+  localStorage.clear();
 }
 
 function filterTask(e) {
