82023f0f1df06cccfa641d8a699e281ccca05330
Anqi-Dai <adai@bu.edu>
2019-01-15 17:15:39 -0500

    changed the button but the fetch is problematic again

--- Basic.javascript.notes/8_04_bored_API/app.js
+++ Basic.javascript.notes/8_04_bored_API/app.js
@@ -4,26 +4,37 @@ const boredAPI = new Bored();
 
 const ui = new UI();
 
-// get easy one
-boredAPI
-  .getActivity("easy", 1, "recreational")
-  .then(activity => {
-    ui.showActivity("easy", activity);
-  })
-  .catch(err => console.log(err));
+// add event listener to the button
+document.getElementById("btn").addEventListener("click", function(e) {
+  // Get the input from the user.
+  const participantsSelected = document.getElementById("participants");
+  const num =
+    participantsSelected.options[participantsSelected.selectedIndex].text;
+  const typeSelected = document.getElementById("type");
+  const type = typeSelected.options[typeSelected.selectedIndex].text;
 
-// get normal one
-boredAPI
-  .getActivity("normal", 1, "recreational")
-  .then(activity => {
-    ui.showActivity("normal", activity);
-  })
-  .catch(err => console.log(err));
+  // Get activities.
+  // get easy one
+  boredAPI
+    .getActivity("easy", 1, "recreational")
+    .then(activity => {
+      ui.showActivity("easy", activity);
+    })
+    .catch(err => console.log(err));
 
-// get hard one
-boredAPI
-  .getActivity("hard", 1, "recreational")
-  .then(activity => {
-    ui.showActivity("hard", activity);
-  })
-  .catch(err => console.log(err));
+  // get normal one
+  boredAPI
+    .getActivity("normal", 1, "recreational")
+    .then(activity => {
+      ui.showActivity("normal", activity);
+    })
+    .catch(err => console.log(err));
+
+  // get hard one
+  boredAPI
+    .getActivity("hard", 1, "recreational")
+    .then(activity => {
+      ui.showActivity("hard", activity);
+    })
+    .catch(err => console.log(err));
+});
